cd368013f7da2ba05c0710bf6103c2a483f23cba
radu-bordea <radu.bordea.dev@gmail.com>
2025-02-22 17:48:41 +0100

    rewrites login, regsiter pages on render

--- src/routes/LoginPage.jsx
+++ src/routes/LoginPage.jsx
@@ -4,6 +4,7 @@ const LoginPage = () => {
   return (
     <div className="flex items-center justify-center h-[calc(100vh-80px)]">
       <SignIn signUpUrl="/register" />
+      login
     </div>
   );
 };
--- src/routes/RegisterPage.jsx
+++ src/routes/RegisterPage.jsx
@@ -4,6 +4,7 @@ const RegisterPage = () => {
   return (
     <div className="flex items-center justify-center h-[calc(100vh-80px)]">
       <SignUp signInUrl="/login" />
+      register
     </div>
   );
 };
