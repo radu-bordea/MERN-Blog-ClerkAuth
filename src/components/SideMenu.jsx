54fc9e60084524af5cc364945e68f61da24983d5
radu-bordea <radu.bordea.dev@gmail.com>
2025-03-19 16:13:58 +0100

    Create Page PostListPage, Search and SideMenu components

---
+++ src/components/SideMenu.jsx
@@ -0,0 +1,68 @@
+import { Link } from "react-router-dom";
+import Search from "./Search";
+
+export default function SideMenu() {
+  return (
+    <div className="px-4 h-max sticky top-8">
+      <h1 className="mb-4 text-sm font-medium">Search</h1>
+      <Search />
+      <h1 className="mt-8 text-sm font-medium">Filter</h1>
+      <div className="flex flex-col gap-2 text-sm">
+        <label htmlFor="" className="flex items-center gap-2 cursor-pointer">
+          <input
+            type="radio"
+            name="sort"
+            value="newest"
+            className="appearance-none w-4 h-4 border-[1.5px] border-amber-800 cursor-pointer rounded-sm bg-white checked:bg-blue-800"
+          />
+          Newest
+        </label>
+        <label htmlFor="" className="flex items-center gap-2 cursor-pointer">
+          <input
+            type="radio"
+            name="sort"
+            value="newest"
+            className="appearance-none w-4 h-4 border-[1.5px] border-amber-800 cursor-pointer rounded-sm bg-white checked:bg-blue-800"
+          />
+          Most Popular
+        </label>
+        <label htmlFor="" className="flex items-center gap-2 cursor-pointer">
+          <input
+            type="radio"
+            name="sort"
+            value="newest"
+            className="appearance-none w-4 h-4 border-[1.5px] border-amber-800 cursor-pointer rounded-sm bg-white checked:bg-blue-800"
+          />
+          Trending
+        </label>
+        <label htmlFor="" className="flex items-center gap-2 cursor-pointer">
+          <input
+            type="radio"
+            name="sort"
+            value="newest"
+            className="appearance-none w-4 h-4 border-[1.5px] border-amber-800 cursor-pointer rounded-sm bg-white checked:bg-blue-800"
+          />
+          Oldest
+        </label>
+      </div>
+      <h1 className="mt-8 text-sm font-medium">Categories</h1>
+      <div className="flex flex-col gap-2 text-sm">
+        <Link className="underline" to="/posts">
+          All
+        </Link>
+        <Link className="underline" to="/posts?cat=development">
+          Development
+        </Link>
+        <Link className="underline" to="/posts?cat=database">
+          Database
+        </Link>
+        <Link className="underline" to="/posts?cat=seo">
+          Search Engines
+        </Link>
+        <Link className="underline" to="/posts?cat=marketing">
+          Marketing
+        </Link>
+      </div>
+    </div>
+  );
+}
